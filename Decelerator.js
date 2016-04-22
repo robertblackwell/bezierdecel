93b4d5eee82058423d28b3f811df6558591f2ac9
ROBERT <rob@whiteacorn.com>
2016-04-21 17:53:23 -0700

    made standalone

--- Decelerator.js
+++ Decelerator.js
@@ -1,4 +1,4 @@
-function(exports){
+(function(exports){
 	function bezier(t, p) {
 	  var order = p.length - 1; // curve order is number of control point - 1
 	  var d = p[0].length;      // control point dimensionality
@@ -167,4 +167,4 @@ function(exports){
 		osd : OSD
 	}
 
-}(typeof exports === 'undefined'? this['Decelerator']={}: exports)
+})(typeof exports === 'undefined'? this['Decelerator']={}: exports)
